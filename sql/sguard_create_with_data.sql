db05a60bb51f76b212de3daab079c5bc064479da
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2015-10-07 14:24:08 +0200

    SQL descargas

--- sql/sguard_create_with_data.sql
+++ sql/sguard_create_with_data.sql
@@ -31,6 +31,40 @@ CREATE TABLE `administradores` (
 
 insert  into `administradores`(`administradorId`,`nombre`,`login`,`password`,`email`) values (2,'MI segundo adm','ad2','ad2',NULL),(4,'Administrador Principal','admin','admin','adm@gh.com'),(5,'Juan Peleon','juan','juan','juan@gmail.com');
 
+/*Table structure for table `descargas` */
+
+DROP TABLE IF EXISTS `descargas`;
+
+CREATE TABLE `descargas` (
+  `descargaId` int(11) NOT NULL AUTO_INCREMENT,
+  `nterminal` varchar(255) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `resultado` text,
+  PRIMARY KEY (`descargaId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `descargas` */
+
+insert  into `descargas`(`descargaId`,`nterminal`,`fecha`,`hora`,`resultado`) values (2,'112339','2015-10-07','13:32:15','CARGA SIMPLE TERMINAL [SIN PROCESO]'),(3,'112339','2015-10-07','13:33:42','CARGA SIMPLE TERMINAL [SIN PROCESO]'),(4,'112339','2015-10-07','13:36:32','CARGA SIMPLE TERMINAL [SIN PROCESO]'),(5,'112339','2015-10-07','14:07:06','CARGA SIMPLE TERMINAL [SIN PROCESO]'),(6,'112339','2015-10-07','14:07:59','CARGA SIMPLE TERMINAL [SIN PROCESO]');
+
+/*Table structure for table `descargas_lineas` */
+
+DROP TABLE IF EXISTS `descargas_lineas`;
+
+CREATE TABLE `descargas_lineas` (
+  `descargaLineaId` int(11) NOT NULL AUTO_INCREMENT,
+  `descargaId` int(11) DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  PRIMARY KEY (`descargaLineaId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `descargas_lineas` */
+
+insert  into `descargas_lineas`(`descargaLineaId`,`descargaId`,`tag`,`fecha`,`hora`) values (1,5,'0004044154','2015-10-07','12:25:13'),(2,5,'0406215258','2015-10-07','13:10:49'),(3,5,'0406213943','2015-10-07','13:10:52'),(4,5,'0403669508','2015-10-07','13:10:55');
+
 /*Table structure for table `edificios` */
 
 DROP TABLE IF EXISTS `edificios`;
@@ -91,11 +125,11 @@ CREATE TABLE `rondas` (
   `tag` varchar(255) DEFAULT NULL,
   `tagf` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`rondaId`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rondas` */
 
-insert  into `rondas`(`rondaId`,`nombre`,`tag`,`tagf`) values (1,'Ronda Exterior 1500',NULL,NULL),(2,'Ronda Interior',NULL,NULL),(11,'La ronda de noche',NULL,NULL);
+insert  into `rondas`(`rondaId`,`nombre`,`tag`,`tagf`) values (1,'Ronda Exterior 1500',NULL,NULL),(2,'Ronda Interior',NULL,NULL),(11,'La ronda de noche','0406215258','0403669508'),(12,'Vamos a ver la ronda','0406215258','0406215258');
 
 /*Table structure for table `rondas_realizadas` */
 
@@ -156,11 +190,11 @@ CREATE TABLE `rondaspuntos` (
   KEY `ref_punto` (`puntoId`),
   CONSTRAINT `ref_punto` FOREIGN KEY (`puntoId`) REFERENCES `puntos` (`puntoId`),
   CONSTRAINT `ref_ronda` FOREIGN KEY (`rondaId`) REFERENCES `rondas` (`rondaId`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rondaspuntos` */
 
-insert  into `rondaspuntos`(`rondaPuntoId`,`orden`,`rondaId`,`puntoId`) values (1,1,1,1),(4,1,2,1),(14,2,1,2),(15,1,11,1),(16,2,11,2);
+insert  into `rondaspuntos`(`rondaPuntoId`,`orden`,`rondaId`,`puntoId`) values (1,1,1,1),(4,1,2,1),(14,2,1,2),(17,1,11,1),(18,2,11,4),(19,2,12,4),(20,3,12,1);
 
 /*Table structure for table `vigilantes` */
 
