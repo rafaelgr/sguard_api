0579b145e7f33a17a64fb3ecb77ca10c02a241cd
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2015-10-08 13:41:20 +0200

    Preparado para instalacion [1]

--- sql/sguard_test.sql
+++ sql/sguard_test.sql
@@ -1,3 +1,2 @@
-SELECT d.descargaId, d.nterminal, d.fecha, d.hora, d.resultado, dl.descargaLineaId, dl.tag, dl.fecha AS lfecha, dl.hora AS lhora, dl.tipo, dl.tipoId
-FROM descargas AS d
-LEFT JOIN descargas_lineas AS dl ON dl.descargaId = d.descargaId
+DELETE FROM rondas_realizadaspuntos;
+DELETE FROM rondas_realizadas;
